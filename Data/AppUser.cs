556c350e7545896796ae87821813884f967f746e
Rafal Hrabia <hrabia.rafal98@gmail.com>
2022-04-11 22:01:20 +0200

    DB

---
+++ Data/AppUser.cs
@@ -0,0 +1,10 @@
+namespace Lira.Data;
+
+public class AppUser
+{
+    public int UserId { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public bool Admin { get; set; }
+}
