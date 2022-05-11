bd28446efb17efaced7e50ad6c1e213a67592e56
Rafał Hrabia <rafixese@gmail.com>
2022-05-11 14:07:31 +0200

    Create Board.cs

---
+++ Lira/Models/Board.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lira.Models
+{
+    public class Board
+    {
+        public Guid BoardId { get; set; }
+        public string Name { get; set; }
+        [Key]
+        [ForeignKey("AspNetUsers")]
+        public Guid UserId { get; set; }
+    }
+}
