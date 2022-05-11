f197ecdb5aa6ba91ac83f1ad1dec9c0249e02730
Rafał Hrabia <rafixese@gmail.com>
2022-05-11 14:12:24 +0200

    board controller

---
+++ Lira/Controllers/BoardController.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lira.Data;
+using Lira.Models;
+
+namespace Lira.Controllers
+{
+    public class BoardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BoardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Board
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Board.ToListAsync());
+        }
+
+        // GET: Board/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Board
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            return View(board);
+        }
+
+        // GET: Board/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Board/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BoardId,Name,UserId")] Board board)
+        {
+            if (ModelState.IsValid)
+            {
+                board.UserId = Guid.NewGuid();
+                _context.Add(board);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(board);
+        }
+
+        // GET: Board/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Board.FindAsync(id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+            return View(board);
+        }
+
+        // POST: Board/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("BoardId,Name,UserId")] Board board)
+        {
+            if (id != board.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(board);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BoardExists(board.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(board);
+        }
+
+        // GET: Board/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var board = await _context.Board
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (board == null)
+            {
+                return NotFound();
+            }
+
+            return View(board);
+        }
+
+        // POST: Board/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var board = await _context.Board.FindAsync(id);
+            _context.Board.Remove(board);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BoardExists(Guid id)
+        {
+            return _context.Board.Any(e => e.UserId == id);
+        }
+    }
+}
