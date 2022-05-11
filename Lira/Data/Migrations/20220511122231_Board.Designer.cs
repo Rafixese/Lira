8a021efa4a0dd6691fe07ff17ac8fa245a0fcb22
Rafał Hrabia <rafixese@gmail.com>
2022-05-11 14:23:49 +0200

    migration Board

--- Lira/Data/Migrations/20220511122231_Board.Designer.cs
+++ Lira/Data/Migrations/20220511122231_Board.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Lira.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Lira.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220511122231_Board")]
+    partial class Board
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
