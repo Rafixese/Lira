26ab83004a144d1e608cf43a69da544e7e8b6571
Rafał Hrabia <rafixese@gmail.com>
2022-05-14 10:56:09 +0200

    fucking finally some progress

--- Lira/Controllers/BoardController.cs
+++ Lira/Controllers/BoardController.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Lira.Data;
 using Lira.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Lira.Controllers
 {
@@ -19,12 +21,15 @@ namespace Lira.Controllers
             _context = context;
         }
 
+        [Authorize]
         // GET: Board
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Board.ToListAsync());
+            var uid = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            return View(await _context.Board.Where(b => b.UserId == uid).ToListAsync());
         }
 
+        [Authorize]
         // GET: Board/Details/5
         public async Task<IActionResult> Details(Guid? id)
         {
@@ -43,6 +48,7 @@ namespace Lira.Controllers
             return View(board);
         }
 
+        [Authorize]
         // GET: Board/Create
         public IActionResult Create()
         {
@@ -54,18 +60,22 @@ namespace Lira.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Create([Bind("Id,Name,UserId")] Board board)
         {
             if (ModelState.IsValid)
             {
                 board.Id = Guid.NewGuid();
+                board.UserId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
                 _context.Add(board);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
             return View(board);
         }
 
+        [Authorize]
         // GET: Board/Edit/5
         public async Task<IActionResult> Edit(Guid? id)
         {
@@ -79,6 +89,7 @@ namespace Lira.Controllers
             {
                 return NotFound();
             }
+
             return View(board);
         }
 
@@ -87,6 +98,7 @@ namespace Lira.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Edit(Guid id, [Bind("Id,Name,UserId")] Board board)
         {
             if (id != board.Id)
@@ -112,12 +124,15 @@ namespace Lira.Controllers
                         throw;
                     }
                 }
+
                 return RedirectToAction(nameof(Index));
             }
+
             return View(board);
         }
 
         // GET: Board/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(Guid? id)
         {
             if (id == null)
@@ -138,6 +153,7 @@ namespace Lira.Controllers
         // POST: Board/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
         {
             var board = await _context.Board.FindAsync(id);
